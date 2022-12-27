b186a74216f3dfad25d1acf8624557c43c1149c0
Manda Putra <mandaputra8@gmail.com>
2022-12-27 08:52:23 +0700

    feat: update deps

--- packages/core/src/lib/core.spec.ts
+++ packages/core/src/lib/core.spec.ts
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react';
 import { useZeroForm, ZeroFormOptions } from './core';
 
 const DATA = { name: 'YOUR NAME' };
